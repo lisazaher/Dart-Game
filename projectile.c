f273543a15c8da5a6b65173c6625cf6c2b2eaab9
lisazaher <zaherlisa@gmail.com>
2019-03-29 12:00:55 -0400

    Added functionality to power and direction bar, can now move up/down and side to side

--- projectile.c
+++ projectile.c
@@ -1,18 +1,34 @@
 #include <stdlib.h>
 volatile int pixel_buffer_start; // global variable
+int direction_position [2] = {0,0};
+int power_position[2] = {0,0};
 
 void clear_screen();
 void draw_line(int x0, int y0, int x1, int y1, short int color);
 void plot_pixel(int x, int y, short int line_color);
 void wait();
 void swap(int * x, int * y);
+void delay();
 
 int main(void)
 {
     volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
     short int colourlist[] = {0xF800, 0x07E0, 0x001F, 0xFFE0, 0xF81F, 0x07FF};
-    int direction_line = 50;
-    int power_line = 10;
+    int i;
+
+    //direction data
+    int direction_line = 50; //length of line
+    const int direction_initial [2] = {5 + direction_line, 220}; //the point of rotation of line
+    direction_position [0] = 2*direction_line + 5;
+    direction_position [1] = direction_initial[1] - direction_line;
+    int position_incx =1;
+
+    //power data
+    int power_line = 20;
+    const int power_initial[2] = {10,150};
+    power_position [0] = power_line + power_initial[0];
+    power_position [1] = power_initial[1];
+    int power_incy = 1;
     int projectile_line = 20;
 
 
@@ -31,10 +47,30 @@ int main(void)
     while (1)
     {
         /* Erase any boxes and lines that were drawn in the last iteration */
-        clear_screen();
+        //clear_screen(); 
 
-        wait(); // swap front and back buffers on VGA vertical sync
+        //animation to move power and direction lines
+        //direction
+        draw_line(direction_initial[0], direction_initial[1], direction_position[0], direction_position[1], colourlist[0]);
+        //power
+        draw_line(power_initial[0], power_position [1], power_position[0], power_position[1], colourlist[1]);
+        //delay
+        wait();
+        //delay();
+        //erase
+        draw_line(direction_initial[0], direction_initial[1], direction_position[0], direction_position[1], 0x0000);
+        draw_line(power_initial[0], power_position [1], power_position[0], power_position[1], 0x0000);
+
+        if (direction_position[0] == 5 || direction_position[0] == 105) position_incx = -position_incx;
+        direction_position[0] += position_incx;
+        
+        if (power_position[1] == 150 || power_position[1] == 100) power_incy = -power_incy;
+        power_position[1] += power_incy;
+        
         pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+        
+        
+       
     }
 }
 
@@ -61,7 +97,15 @@ void clear_screen() {
     int y;
     for (x = 0; x < 320; x++)
         for (y = 0; y < 240; y++)
-            plot_pixel(x, y, 0x0000);   
+            plot_pixel(x, y, 0x0000);  
+        
+}
+
+void delay() {
+    volatile int * timer_ptr = 0xFFFEC600;
+    int delay = 100000000;
+    int i=0;
+    while (i!=delay) i++;
 }
 
 void plot_pixel(int x, int y, short int line_color){
@@ -106,6 +150,14 @@ void draw_line(int x0, int y0, int x1, int y1, short int color) {
         }
     } 
 }
+
+
+
+
+
+
+
+
 
 
 
